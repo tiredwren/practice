ae82ee84aba32a29be94c68c52ba73c6b1aeb6d2
tiredwren <moudgalya.sneha@gmail.com>
2024-06-16 19:26:06 -0700

    day 2

---
+++ day2.py
@@ -0,0 +1,51 @@
+# user_feeling = input("how was your day? ")
+# print(list[2])
+
+
+# # one solution
+
+# words = []
+# noun = input("please give me a noun: ")
+# adjective = input("please give me a adjective: ")
+# words.append(noun)
+# words.append(adjective)
+# print("the " + words[1] + " food is " + words[0])
+
+
+
+# conditionals
+
+color = "blue"
+
+if color == "red":
+    print("color is red")
+elif color == "blue":
+    print("1")
+else:
+    print("the color is not green or red")
+
+x = 1
+if x >= 0:
+    pass
+
+if x == 0:
+    pass
+
+if x <= 0:
+    pass
+
+if x!=0:
+    pass
+# and
+if x>0 & x!=1:
+    pass
+# or
+if x<0 | x==1:
+    pass
+
+# operations
+# != : not equal to
+# == : equal to
+# >= : greater than or equal to
+# <= : less than or equal to
+
